35c3fa12726dfe7d3c3d98689e4985317ea25510
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-07 01:34:53 +0300

    Add a program to show us the print function tricks

---
+++ Basic/Print_Tricks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Python program to show the tricks we can use in the 
+                    print function inside Python
+================================================================================'''
+
+print('Ahmed "Adel" Youssef')
+print("Ahmed 'Adel' Youssef")
+print('''His name is "Ahmed" and he is '30' Years old ''')
