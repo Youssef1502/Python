4230ff080364d9777b7a3abd1e926ba971a71053
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-08 23:00:19 +0300

    Add this program to print the text in the figlet shape

---
+++ Basic/Figlet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Print Text in figlet format
+================================================================================'''
+
+import pyfiglet
+
+result = pyfiglet.figlet_format("Youssef Adel")
+print(result)
