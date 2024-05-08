670100af62dd7f39b4384f294ec804851305157d
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-08 23:10:00 +0300

    Enter a sentence and then prints it in the opposite direction

---
+++ Basic/Reverse_String.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Python code that asks the user to enter a sentence and then 
+                    prints it in the opposite direction
+================================================================================'''
+
+Name = input("Please Enter Name :")
+txt = Name[::-1]
+print(txt)
