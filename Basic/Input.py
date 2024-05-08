b9465b18e2bcfceba89015e4829c6126dbf003c0
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-08 23:05:42 +0300

    This program describes how you can get input from the user

---
+++ Basic/Input.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Python program to show us how we can receive input 
+                    from the user
+================================================================================'''
+
+print("Please Enter Your Name : ")
+Name = input()
+print("Hello " + Name)
+
+print("-------------------")
+
+print("Please Enter Your Age : ")
+Age = int(input())
+print("My Age is : {}" .format(Age))
