8380af8dbd88eba99b0aeeccceb858c138bdaf9c
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-11 23:49:44 +0300

    Python program for how to use lambda expression

---
+++ Basic/Lambda_Expression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Python program for how to use lambda expression
+================================================================================'''
+
+x = lambda a , b : a * b
+
+print( x(2 , 3) )
