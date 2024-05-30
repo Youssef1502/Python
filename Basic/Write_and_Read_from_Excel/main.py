5fe19ba591e80a70a84e82a70e34e46a95166dce
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-31 00:31:45 +0300

    Python program for Write and Read from Excel sheet

---
+++ Basic/Write_and_Read_from_Excel/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Python program for Write and Read from Excel sheet
+================================================================================'''
+
+import openpyxl
+
+
+def Read_Excel_File(File_Path):
+    workbook = openpyxl.load_workbook(File_Path)
+    sheet = workbook.active
+    
+    for row in sheet.iter_rows(values_only = True):
+        print(row)
+        
+    workbook.close()
+
+
+def Write_Excel_File(File_Path):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    
+    data = [
+        ["Name" , "Age" , "City"],
+        ["Ahmed" , "31" , "Egypt"],
+        ["John" , "28" , "New Yourk"],
+        ["Youssef" , "27" , "Egypt"]
+    ]
+    
+    for row in data:
+        sheet.append(row)
+        
+    workbook.save(File_Path)
+    workbook.close()
+
+
+Write_Excel_File("Example.xlsx")
+Read_Excel_File("Example.xlsx")
