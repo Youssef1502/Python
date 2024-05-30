cf2f3cc794253bbb0d672d8d67a6ffc7f51b2641
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-31 00:25:40 +0300

    Make some files containing a set of functions you want to include in your application

---
+++ Basic/Modules/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Make some files containing a set of functions you want to include in your application.
+================================================================================'''
+
+from Utils1 import *
+import Utils2.Utils2 as Fun2
+
+x = Sum(5,6)
+print(x)
+
+x = Sub(20,7)
+print(x)
+
+x = Fun2.Mul(5,6)
+print(x)
+
+x = Fun2.Div(20,4)
+print(x)
