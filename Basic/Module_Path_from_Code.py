ee578301ffbb8e0297f38119d9201d965780af7b
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-05-31 09:20:55 +0300

    Write an application to get the module path from the code

---
+++ Basic/Module_Path_from_Code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Write an application to get the module path from the code
+================================================================================'''
+
+import importlib
+print("Location of keyboard module source :")
+print(importlib.import_module('keyboard'))
